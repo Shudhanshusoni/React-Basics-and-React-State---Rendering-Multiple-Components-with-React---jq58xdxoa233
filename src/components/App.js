e053987139726ec2dc50e0d877ea0a8953951413
Shudhanshusoni <97749507+Shudhanshusoni@users.noreply.github.com>
2022-05-04 16:50:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">E-commerce App</h1>
+      <h2 data-ns-test="project-description">online buying and selling app</h2>
+    </div>
   )
 }
 
