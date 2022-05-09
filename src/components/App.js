46548102cfc28e59cd446cf67c4f6a2c04782f14
Shudhanshusoni <97749507+Shudhanshusoni@users.noreply.github.com>
2022-05-09 14:40:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,26 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
+  const project=[
+    {
+      id:1,
+      name:'project-name'
+    },
+    {
+      id:2,
+      name:'project-description'
+    }
+  ]
   return (
     <div id="main">
-      <div>
-      <h1 data-ns-test="project-name">project name</h1>
-      </div>
-      <div>
-      <h2 data-ns-test="project-description"> project description</h2>
-      </div>
+      { project.map((cur) => (
+          <div>
+          <h1 data-ns-test="project-name">{project.name}</h1>
+          <h2 data-ns-test="project-description"> {project.name}</h2>
+          </div>
+        ))} 
     </div>
   )
 }
-
 
 export default App;
